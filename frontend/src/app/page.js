1447b86d85dd9e21754296e1224859aa3785c5ed
Aditya Vikram Chawla <chawlaadityavikram@gmail.com>
2026-04-16 22:08:51 +0530

    final homepage fix before deploy

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,7 @@
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  redirect("/dashboard");
+  return (
+    <div>
+      <a href="/dashboard">Go to Dashboard</a>
+    </div>
+  );
 }
